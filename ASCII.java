e07eb1f8418cd00d63164ee1a7a67a2dd40ace6d
Isabel Siva <bell.cristina.pereiradasilva@gmail.com>
2022-06-03 23:21:37 -0300

    exercises chapter 3

---
+++ ASCII.java
@@ -0,0 +1,14 @@
+import java.io.IOException;
+
+public class ASCII {
+    public static void main(String []args) throws IOException {
+        char ascii;
+        System.out.println("Digite qualquer letra" +
+                " para parar digite '.'");
+        do{
+            ascii = (char)System.in.read();
+            ascii -= 32;
+            System.out.println(ascii);
+        }while (ascii != '.');
+    }
+}
---
+++ Pg.java
@@ -0,0 +1,8 @@
+public class Pg {
+    public static void main(String []args){
+        for (int i = 0; i <= 100;){
+            System.out.println(i);
+            i += 2;
+        }
+    }
+}
